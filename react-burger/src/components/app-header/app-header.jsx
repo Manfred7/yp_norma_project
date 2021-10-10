9297646b98f5dab6ae261b86d047418b07c14b34
Manfred7 <manfred7@yandex.ru>
2021-10-10 17:18:14 +0300

    AppHeader переделал на функциональную компоненту

--- react-burger/src/components/app-header/app-header.jsx
+++ react-burger/src/components/app-header/app-header.jsx
@@ -1,39 +1,39 @@
 import {BurgerIcon, ListIcon, Logo, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
-import React, {Component} from 'react';
+import React  from 'react';
 import s from "./app-header.module.css"
 
-class AppHeader extends Component {
-    render() {
-        return (
-            <header className={s.mainHeader}>
-                <nav className={s.mainHeaderNav + " " + s.container}>
-                    <ul className={s.siteNavigation}>
-                        <li className={s.siteNavigationItem}>
-                            <BurgerIcon type="primary"/>
-                            <a className={s.linkItem} href="index.html">Конструктор</a>
-                        </li>
-                        <li className={s.siteNavigationItem}>
-                            <ListIcon type="primary"/>
-                            <a className={s.linkItem} href="index.html">Лента заказов</a>
-                        </li>
-                    </ul>
+const AppHeader = () => {
 
-                    <a className={s.mainHeaderLogo} href="index.html">
-                        <Logo/>
-                    </a>
+    return (
+        <header className={s.mainHeader}>
+            <nav className={s.mainHeaderNav + " " + s.container}>
+                <ul className={s.siteNavigation}>
+                    <li className={s.siteNavigationItem}>
+                        <BurgerIcon type="primary"/>
+                        <a className={s.linkItem} href="index.html">Конструктор</a>
+                    </li>
+                    <li className={s.siteNavigationItem}>
+                        <ListIcon type="primary"/>
+                        <a className={s.linkItem} href="index.html">Лента заказов</a>
+                    </li>
+                </ul>
 
-                    <ul className={s.siteNavigation}>
-                        <li className={s.siteNavigationItem}>
-                            <ProfileIcon type="primary"/>
-                            <a className={s.linkItem} href="index.html">Личный кабинет</a>
-                        </li>
-                    </ul>
+                <a className={s.mainHeaderLogo} href="index.html">
+                    <Logo/>
+                </a>
 
-                </nav>
-            </header>
+                <ul className={s.siteNavigation}>
+                    <li className={s.siteNavigationItem}>
+                        <ProfileIcon type="primary"/>
+                        <a className={s.linkItem} href="index.html">Личный кабинет</a>
+                    </li>
+                </ul>
 
-        )
-    }
+            </nav>
+        </header>
+
+    )
 }
+
 
 export default AppHeader;
